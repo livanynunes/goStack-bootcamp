1b0dd3cea3993bf48f23c0127c881b77021fb480
livany.tavares <livany.tavares@clubpetro.com>
2020-04-04 23:09:46 -0300

    Environment, concepts user register

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 // import User from './app/models/User';
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionControler';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -15,5 +18,8 @@ const routes = new Router();
 // });
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
